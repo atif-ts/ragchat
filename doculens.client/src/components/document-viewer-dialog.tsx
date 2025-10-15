2b6e5f6b2d2d79456827db7e1f96943532a6bbb4
Sanya Saeed <sania.saeedsefam@gmail.com>
2025-10-15 14:50:02 +0500

    Disable preview for .doc/.docx files in CitationLink; keep download only

--- doculens.client/src/components/document-viewer-dialog.tsx
+++ doculens.client/src/components/document-viewer-dialog.tsx
@@ -107,7 +107,7 @@ export const DocumentViewerDialog: React.FC<DocumentViewerDialogProps> = ({
     }] : [];
 
     return (
-        <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4">
+        <div className="fixed inset-0 bg-black/40 z-50 flex items-center justify-center p-4">
             <div className="bg-white rounded-lg shadow-xl w-full max-w-6xl h-[90vh] flex flex-col">
                 {/* Header */}
                 <div className="flex items-center justify-between p-4 border-b border-gray-200 flex-shrink-0">
